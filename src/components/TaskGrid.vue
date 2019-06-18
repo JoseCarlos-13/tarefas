1fb2ab222df555a544d2428f35372a9338d03b86
José Carlos <jose.carlos@safetysystemtechnology.com.br>
2019-06-18 17:53:52 -0300

    os botões foram novamente modificados

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
        <template>
-			<div v-for="task in tasks" :key="task.name" class="task">
-				<p>{{task.action}}</p>
+			<div v-for="task in tasks" :key="task.name" >
+				<button class="task"><p>{{task.action}}</p></button>
 			</div>
 		</template>
     </div>
@@ -24,11 +24,15 @@ export default {
 
 <style>
     .task{
-        box-shadow: 4px 8px 2px black;
-        border-radius: 8px;
+        box-shadow: 4px 8px 5px black;
+        border-radius: 12px;
         padding: 15px;
         background-color: darkgreen;
-        border-right: 6px solid black;
+        border-right: 8px solid black;
+        border-bottom: 5px solid black;
+        border-top: 0px solid black;
+        border-left: 0px solid black;
+        color: white;
         font-size: 14pt;
         margin: 20px;
         font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
