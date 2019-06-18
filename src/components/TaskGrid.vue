7fedc0c17af0a30b425078ab4d039f44c7f2b20b
José Carlos <jose.carlos@safetysystemtechnology.com.br>
2019-06-18 17:29:06 -0300

    botões estilizados

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
        <template>
-			<div v-for="task in tasks" :key="task.name">
-				<p>{{task.name}}</p>
+			<div v-for="task in tasks" :key="task.name" class="task">
+				<p>{{task.action}}</p>
 			</div>
 		</template>
     </div>
@@ -23,7 +23,16 @@ export default {
 </script>
 
 <style>
-
+    .task{
+        box-shadow: 4px 8px 2px black;
+        border-radius: 8px;
+        padding: 15px;
+        background-color: darkgreen;
+        border-right: 5px solid black;
+        font-size: 14pt;
+        margin: 20px;
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
 </style>
 
 
