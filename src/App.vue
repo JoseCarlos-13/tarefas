71f02d0fce48365f1365507ea0e824e110c738b2
José Carlos <jose.carlos@safetysystemtechnology.com.br>
2019-06-18 16:06:00 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+		<tasks-grid/>
+	</div>
+</template>
+
+<script>
+import Tasks from './components/TaskGrid'
+export default {
+	components: {
+		'tasks-grid': Tasks
+	},
+	props: ['tasks']
+}
+</script>
+
+<style>
+	body {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		background: linear-gradient(to right, rgb(16, 31, 19), rgb(36, 77, 68));
+		color: rgb(255, 255, 255);
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 4rem;
+	}
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <template>
+			<div v-for="task in tasks" :key="task.name">
+				<p>{{task.name}}</p>
+			</div>
+		</template>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return{
+            tasks: [
+                { name: "Race", action: "run in the street"},
+                { name: "Cook", action: "cook a dinner"}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
